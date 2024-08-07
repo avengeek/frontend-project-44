d1a3704060646af22386be6e66436c29504312c7
Andrey Gerasimuk <andrey@gerasimuk.pro>
2024-08-07 19:52:39 +0300

    game brain-even is working

---
+++ bin/brain-even.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+import hello from '../src/cli.js';
+import getRandomNumber from './getrandomnumber.js';
+
+console.log('Welcome to the Brain Games!');
+
+const playerName = hello();
+
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+let answersCount = 0;
+
+while (answersCount < 3) {
+  const randomNumber = getRandomNumber();
+  const rightAnswer = (randomNumber % 2) ? 'no' : 'yes';
+  answersCount += 1;
+
+  console.log(`${answersCount} question: ${randomNumber}`);
+
+  const playerAnswer = readlineSync.question('Your answer: ');
+
+  if (rightAnswer === playerAnswer) {
+    console.log('Correct!');
+  } else {
+    console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
+    break;
+  }
+  console.log(`Congratulations, ${playerName}`);
+}
---
+++ bin/getrandomnumber.js
@@ -0,0 +1,6 @@
+const getRandomNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 98) + 2;
+  return randomNumber;
+};
+
+export default getRandomNumber;
