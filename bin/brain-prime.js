707a6668ce8b246fe37c22918e5740e71ea7635a
Andrey Gerasimuk <andrey@gerasimuk.pro>
2024-08-12 22:58:27 +0300

    correct Congratulations string

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -23,7 +23,7 @@ while (answersCount < 3) {
     console.log('Correct!');
     answersCount += 1;
     if (answersCount === 3) {
-      console.log(`Congratulations, ${playerName}`);
+      console.log(`Congratulations, ${playerName}!`);
     }
   } else {
     console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
